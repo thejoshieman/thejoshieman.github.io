bb4d72b2ad4e4159684d59f42cd45138ca699de2
Usi'ia Lior Diamond <usidiamond@proton.me>
2024-12-08 21:06:18 -0500

    fix test

--- src/app/left/left.component.spec.ts
+++ src/app/left/left.component.spec.ts
@@ -1,23 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { LeftComponent } from './left.component';
+import { LeftComponent } from "./left.component";
 
-describe('LeftComponent', () => {
+describe("LeftComponent", () => {
   let component: LeftComponent;
   let fixture: ComponentFixture<LeftComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       imports: [LeftComponent]
-    })
-    .compileComponents();
-
+    });
     fixture = TestBed.createComponent(LeftComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });

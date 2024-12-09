76a081633aadb8fe6ac6117152509260a7be1037
Usi'ia Lior Diamond <usidiamond@proton.me>
2024-12-08 21:11:24 -0500

    add back in headless

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: [ 'ChromeHeadlessCI'],
+    browsers: [ 'ChromeHeadless', 'ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',

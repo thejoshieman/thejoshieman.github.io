b5214acba5fda83ed0c411b79cba3df0e9f28217
Usi'ia Lior Diamond <usidiamond@proton.me>
2024-12-08 20:57:37 -0500

    Grids adjustment and content

---
+++ src/app/left/left.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left',
+  imports: [],
+  templateUrl: './left.component.html',
+  styleUrl: './left.component.css'
+})
+export class LeftComponent {
+
+}

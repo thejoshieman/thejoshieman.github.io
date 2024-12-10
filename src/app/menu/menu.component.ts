51779c0e1805d21f9aee1bca9cb4e8eedaa0a7d0
Usi'ia Lior Diamond <usidiamond@proton.me>
2024-12-10 13:30:25 -0500

    reformat site and do css fixes

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
-import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: "app-menu",
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: "./menu.component.html",
   styleUrl: "./menu.component.css",
 })

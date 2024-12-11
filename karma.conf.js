83e8585a60661d4d882f0fd4bd7e745377ff1fb7
Usi'ia Lior Diamond <usidiamond@proton.me>
2024-12-11 17:21:43 -0500

    alter script

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function (config) {
     browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
-        base: 'ChromeHeadlessCI',
+        base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       },
     },

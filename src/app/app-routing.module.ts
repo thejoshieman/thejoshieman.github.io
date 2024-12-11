a8fe509052711d5b7239b684f4f29d3e92b938f0
Usi'ia Lior Diamond <usidiamond@proton.me>
2024-12-11 15:22:35 -0500

    Fix unit tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { FaqComponent } from "./faq/faq.component";
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "faq", component: FaqComponent },
   { path: "", redirectTo: "home", pathMatch: "full" },

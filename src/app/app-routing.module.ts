6cab213f5ef259b961f280a2363176317fc67c7b
Usi Diamond <28960829+UsiDiamond@users.noreply.github.com>
2025-04-09 00:50:22 -0700

    add back home (#138)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,16 @@ import { CookingComponent } from './cooking/cooking.component';
 import { PeopleComponent } from './people/people.component';
 import { EventsComponent } from './events/events.component';
 import { LifeComponent } from './life/life.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'talker', component: TalkerComponent },
   { path: 'cooking', component: CookingComponent },
   { path: 'people', component: PeopleComponent },
   { path: 'events', component: EventsComponent },
   { path: 'life', component: LifeComponent },
-  { path: '', redirectTo: 'talker', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
 @NgModule({
